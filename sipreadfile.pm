e36d889c216d8fae7b58dbd31a58334918bff6ab
bigroot888 <etc-10@inbox.ru>
2021-11-22 11:31:19 +0300

    initial commit

---
+++ sipreadfile.pm
@@ -0,0 +1,23 @@
+package SipReadFile;
+use strict;
+use warnings;
+
+use Exporter qw(import);
+
+sub ReadFile() {
+    open F, '<' ,'/etc/asterisk/sip.conf';
+    my @num;
+    while(<F>) {
+	my $row = $_;
+	next if $row =~ /^\;/;
+	next if $row =~ /^\[general\]/;
+	next if $row =~ /\(\!\)/;
+	if ($row =~ /^\[[0-9]*|[a-zA-Z]*\]/) {
+	    push(@num, substr($row,1,index($row,']')-1));
+	}
+    }
+    close F;
+    return @num;
+}
+
+1;
